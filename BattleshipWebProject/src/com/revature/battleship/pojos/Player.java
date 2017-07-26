05174078fdc9029b4646a34dd3692646eb339e3f
xanieros <shawnba91@gmail.com>
2017-07-26 11:58:41 -0400

    Added pojo package and maven

---
+++ BattleshipWebProject/src/com/revature/battleship/pojos/Player.java
@@ -0,0 +1,13 @@
+package com.revature.battleship.pojos;
+
+import java.sql.Blob;
+
+public class Player {
+	private String uname;
+	private String pword;
+	private String email;
+	private int uid;
+	private String fname;
+	private String lname;
+	private String profPic;
+}
